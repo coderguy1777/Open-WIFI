ed0049f0911becf9ca4996605632d69c7eeb0d10
coderguy1777 <hill.jordan@charterschool.org>
2019-10-03 18:03:24 -0400

    added files to fill repo.

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import requests
-import numpy as np
-def main():
+import xmltodict as xml
+
---
+++ nodes/user.py
@@ -0,0 +1,5 @@
+import requests as rq
+
+class User:
+    def __init__(self):
+        self.user_name = 0
