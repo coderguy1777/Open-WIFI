57541fb76c43decba823059c4c63dbca02fa9975
coderguy1777 <hill.jordan@charterschool.org>
2019-10-04 12:43:08 -0400

    tmmr set up nodes of the p2p

--- nodes/user.py
+++ nodes/user.py
@@ -1,5 +1,6 @@
 import requests as rq
 
 class User:
-    def __init__(self):
-        self.user_name = 0
+    def __init__(self, ip, dev_name):
+        self.ip = ip
+        self.dev_name = dev_name
